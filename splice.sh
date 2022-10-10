92a4ab1644184b1ee8cceb3d8457edb272d1b138
CalNightingale <cal.nightingale@gmail.com>
2022-10-10 16:48:32 -0400

    added comments for splicing

--- splice.sh
+++ splice.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Create list of all clips to splice
 ls -1 staging | sed 's/^/file /' >> clips.txt
 
+# Put list in staging directory so it is properly wiped on new splice
 mv clips.txt staging
 
+# execute concatenation with ffmpeg
 ffmpeg -f concat -i staging/clips.txt -c copy staging/__merged.MP4
